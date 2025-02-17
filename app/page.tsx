00b18ca2ce211131b933d74336e62e5e1b3d74c8
maikynata <maikynata25@gmail.com>
2025-02-17 17:12:40 -0300

    shadcn import component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <h1 className="text 2xl">Dashboard</h1>
+      <Button>
+        Click me
+      </Button>
     </>
   );
 }
